2df82103329d8bcce700be49ca0cbfe3d3e08f6b
DELL <DELL@192.168.0.13>
2020-09-19 18:56:33 +0530

    Commit ten

--- Assignment/src/main/java/com/aish/assignment1/Compare.java
+++ Assignment/src/main/java/com/aish/assignment1/Compare.java
@@ -1,5 +1,20 @@
 package com.aish.assignment1;
-
-public class Compare {
-
+import java.util.Scanner;
+public class Compare 
+{
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        n = s.nextInt();
+        if(n < 0)
+        {
+            System.out.println("The given number "+n+" is Negative");
+        }
+        else {
+        	System.out.println("The given Number "+n+" is not Negative");
+        }
+        s.close();
+    }
 }

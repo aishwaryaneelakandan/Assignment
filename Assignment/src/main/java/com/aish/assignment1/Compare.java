d9a666ca436624141c8180676e8f8f19bc13c404
DELL <DELL@192.168.0.13>
2020-09-25 18:39:27 +0530

    Commit sixteen

--- Assignment/src/main/java/com/aish/assignment1/Compare.java
+++ Assignment/src/main/java/com/aish/assignment1/Compare.java
@@ -1,97 +1,106 @@
 package com.aish.assignment1;
 public class Compare 
 {
-    public boolean negative(int a) {
-    	if(a<0)
-    	{
-    		return true;
-    	}
-    	else
-    	{
-    		return false;
-    	}
-    }
-    	
-    public boolean equal(int a,int b) {
-	    if(a==b){
-	    	return true;
-	      }
-	    else{
-	    	return false;
-	    }
+	public boolean negative(int a) {
+		if (a < 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-    public boolean isequal(String str1,String str2){
-    	if(str1==str2) {
-    		return true;
-    	}
-    	else {
-    		return false;
-    	}
-    }
-    public boolean equals(String str1,String str2){
-    	boolean bool=str1.equalsIgnoreCase(str2);
-    	if(bool) {
-    		return true;
-    	}
-    	else {
-    		return false;
-    	}
-    	
-    }
-    public boolean even(int num) {
-    	if(num%2==0) {
-    		return true;
-    	}
-    	else {
-    		return false;
-    	}
-    }
-    public boolean odd(int num) {
-    	if(num%2!=0) {
-    		return true;
-    	}
-    	else {
-    		return false;
-    	}
-    }
-    public boolean palindrome(String str) {
-    	String rev="";
-    	int length = str.length();
-    	 
-        for ( int i = length - 1; i >= 0; i-- )
-           rev = rev + str.charAt(i);
-   
-        if (str.equals(rev)) {
-        	return true;
-        }
-        else {
-        	return false;
-        }
-    }
-    public boolean isStringOnlyAlphabet(String str) {
-    	return ((!str.equals("")) 
-                && (str != null) 
-                && (str.matches("^[a-zA-Z]*$")));
-    }
-    public String stringTimes(String str, int n) {
-    	String abc="";
-    	for (int i=0;i<n; i++){
-    	abc=abc+str;
-    	}
-    	return abc;
-    	}
-    public String frontTimes(String str, int n) {
-    	String def="";
-    	for(int m=0;m<n;m++){
-    	if(str.length()>=3)
-    		def=str;
-    	if(str.length()>3)
-    		def=str.substring(0,3);
-    	}
-    	return def;
-    	}
- }
 
+	public boolean equal(int a, int b) {
+		if (a == b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
+	public boolean isequal(String str1, String str2) {
+		if (str1 == str2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
+	public boolean equals(String str1, String str2) {
+		boolean bool = str1.equalsIgnoreCase(str2);
+		if (bool) {
+			return true;
+		} else {
+			return false;
+		}
 
+	}
+
+	public boolean even(int num) {
+		if (num % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean odd(int num) {
+		if (num % 2 != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean palindrome(String str) {
+		String rev = "";
+		int length = str.length();
+
+		for (int i = length - 1; i >= 0; i--)
+			rev = rev + str.charAt(i);
+
+		if (str.equals(rev)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean isStringOnlyAlphabet(String str) {
+		return ((!str.equals("")) && (str != null) && (str.matches("^[a-zA-Z]*$")));
+	}
+
+	public String stringTimes(String str, int n) {
+		String abc = "";
+		for (int i = 0; i < n; i++) {
+			abc = abc + str;
+		}
+		return abc;
+	}
+
+	public String frontTimes(String str, int n) {
+		String result = "";
+		for (int i = 0; i < n; i++) {
+			if (str.length() <= 3)
+				result += str;
+			if (str.length() > 3)
+				result += str.substring(0, 3);
+		}
+		return result;
+	}
+
+	public boolean array123(int[] number) {
+		for (int i = 0; i < number.length - 2; i++) {
+			if (number[i] == 1 && number[i + 1] == 2 && number[i + 2] == 3)
+				return true;
+		}
+		return false;
+	}
+
+	public String stringSplosion(String str) {
+		String n = "";
+		for (int i = 0; i < str.length(); i++) {
+			n += str.substring(0, i + 1);
+		}
+		return n;
+	}
+}
--- Assignment/src/test/java/com/aish/assignment1/CompareTest.java
+++ Assignment/src/test/java/com/aish/assignment1/CompareTest.java
@@ -8,65 +8,84 @@ class CompareTest {
 
 	@Test
 	void testa() {
-		
-		Compare s=new Compare();
+		Compare s = new Compare();
 		assertTrue(s.negative(-100));
 	}
+
 	@Test
 	void testequal() {
-		
-		Compare s=new Compare();
+        Compare s = new Compare();
 		assertTrue(s.equal(2, 2));
 	}
+
 	@Test
 	void testc() {
-		
-		Compare s=new Compare();
+        Compare s = new Compare();
 		assertTrue(s.isequal("hello", "hello"));
 	}
-	
+
 	@Test
 	void testd() {
-		Compare s=new Compare();
+		Compare s = new Compare();
 		assertTrue(s.equals("Hello", "hello"));
 	}
+
 	@Test
 	void teste() {
-		Compare s=new Compare();
+		Compare s = new Compare();
 		assertTrue(s.even(2));
 	}
+
 	@Test
 	void testf() {
-		Compare s=new Compare();
+		Compare s = new Compare();
 		assertTrue(s.odd(7));
 	}
+
 	@Test
 	void testg() {
-		Compare s=new Compare();
+		Compare s = new Compare();
 		assertTrue(s.palindrome("lol"));
 	}
+
 	@Test
 	void testh() {
-		Compare s=new Compare();
+		Compare s = new Compare();
 		assertTrue(s.isStringOnlyAlphabet("mango"));
 	}
+
 	@Test
 	void testi() {
-		Compare s=new Compare();
-		assertNotNull(s.stringTimes("Hi", 2));
-		assertNotNull(s.stringTimes("Hi", 3));
-		assertNotNull(s.stringTimes("Hi", 1));
+		Compare s = new Compare();
+		assertEquals(s.stringTimes("Hi", 2), "HiHi");
+		assertEquals(s.stringTimes("Hi", 3), "HiHiHi");
+		assertEquals(s.stringTimes("Hi", 1), "Hi");
 	}
+
 	@Test
 	void testj() {
-		Compare s=new Compare();
-		assertNotNull(s.frontTimes("Computer", 2));
-		assertNotNull(s.frontTimes("Computer", 3));
-		assertNotNull(s.frontTimes("Ram", 3));
+		Compare s = new Compare();
+		assertEquals(s.frontTimes("Chocolate", 2), "ChoCho");
+		assertEquals(s.frontTimes("Chocolate", 3), "ChoChoCho");
+		assertEquals(s.frontTimes("Abc", 3), "AbcAbcAbc");
 	}
+
+	@Test
+	void testk() {
+		Compare s = new Compare();
+		int a[] = { 1, 1, 2, 3, 1 };
+		int b[] = { 1, 1, 2, 4, 1 };
+		int c[] = { 1, 1, 2, 1, 2, 3 };
+		assertTrue(s.array123(a));
+		assertTrue(s.array123(b));
+		assertTrue(s.array123(c));
 	}
 
-
-
-
-
+	@Test
+	void testl() {
+		Compare s = new Compare();
+		assertEquals(s.stringSplosion("Code"), "CCoCodCode");
+		assertEquals(s.stringSplosion("abc"), "aababc");
+		assertEquals(s.stringSplosion("ab"), "aab");
+	}
+}

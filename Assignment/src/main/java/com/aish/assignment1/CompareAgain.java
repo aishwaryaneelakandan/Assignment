300768543fa93b467a67375e956f5090312a4685
DELL <DELL@192.168.1.6>
2020-10-03 18:01:56 +0530

    Commit Thirty Four

--- Assignment/src/main/java/com/aish/assignment1/CompareAgain.java
+++ Assignment/src/main/java/com/aish/assignment1/CompareAgain.java
@@ -114,4 +114,152 @@ public class CompareAgain {
 			result[i] = start++;
 		return result;
 	}
+
+	public int[] pre4(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 4 && i > 0) {
+				int[] foo;
+				foo = new int[i];
+				for (int j = 0; j < foo.length; j++) {
+					foo[j] = nums[j];
+				}
+				if (nums[0] != 4)
+					return foo;
+			}
+		}
+		int[] bar;
+		bar = new int[0];
+		return bar;
+	}
+
+	public int[] zeroFront(int[] nums) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				nums[i] = nums[count];
+				nums[count] = 0;
+				count++;
+			}
+		}
+		return nums;
+	}
+
+	public int[] evenOdd(int[] nums) {
+		int countE = 0;
+		int countO = nums.length - 1;
+		int[] array = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] % 2 == 0) {
+				array[countE] = nums[i];
+				countE++;
+			} else {
+				array[countO] = nums[i];
+				countO--;
+			}
+		}
+		return array;
+	}
+
+	public int maxSpan(int[] nums) {
+		int span = 0;
+		int tmp = 0;
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				if (nums[i] == nums[j]) {
+					tmp = j - i + 1;
+					span = Math.max(tmp, span);
+				}
+			}
+		}
+		return span;
+	}
+
+	public boolean canBalance(int[] nums) {
+		int lSum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			lSum += nums[i];
+			int rSum = 0;
+			for (int j = nums.length - 1; j > i; j--) {
+				rSum += nums[j];
+			}
+			if (rSum == lSum)
+				return true;
+		}
+		return false;
+	}
+
+	public int[] seriesUp(int n) {
+		int[] result = new int[n * (n + 1) / 2];
+		int i = 0;
+		for (int j = 1; j <= n; ++j)
+			for (int k = 1; k <= j; ++k)
+				result[i++] = k;
+		return result;
+	}
+
+	public int[] fix34(int[] nums) {
+		int j = 1;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 3 && nums[i + 1] != 4) {
+				for (; nums[j] != 4; j++)
+					;
+				nums[j] = nums[i + 1];
+				nums[i + 1] = 4;
+			}
+		}
+		return nums;
+	}
+
+	public boolean linearIn(int[] outer, int[] inner) {
+		int numFound = 0;
+		if (inner.length == 0) {
+			return true;
+		}
+		int k = 0;
+		for (int i = 0; i < outer.length; i++) {
+			if (outer[i] == inner[k]) {
+				numFound++;
+				k++;
+			} else if (outer[i] > inner[k]) {
+				return false;
+			}
+			if (numFound == inner.length)
+				return true;
+		}
+		return false;
+	}
+
+	public int maxMirror(int[] nums) {
+		int len = nums.length;
+		int count = 0;
+		int max = 0;
+		for (int i = 0; i < len; i++) {
+			count = 0;
+			for (int j = len - 1; i + count < len && j > -1; j--) {
+				if (nums[i + count] == nums[j]) {
+					count++;
+				} else {
+					if (count > 0) {
+						max = Math.max(count, max);
+						count = 0;
+					}
+				}
+			}
+			max = Math.max(count, max);
+		}
+		return max;
+	}
+
+	public int[] fix45(int[] nums) {
+		int j = 0;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 4 && nums[i + 1] != 5) {
+				for (; !(nums[j] == 5 && (j == 0 || j > 0 && nums[j - 1] != 4)); j++)
+					;
+				nums[j] = nums[i + 1];
+				nums[i + 1] = 5;
+			}
+		}
+		return nums;
+	}
 }
--- Assignment/src/test/java/com/aish/assignment1/CompareAgainTest.java
+++ Assignment/src/test/java/com/aish/assignment1/CompareAgainTest.java
@@ -109,4 +109,113 @@ public class CompareAgainTest {
 		assertArrayEquals(new int[] { 1, 2 }, s1.fizzArray3(1, 3));
 	}
 
+	@Test
+	void testk() {
+		CompareAgain s1 = new CompareAgain();
+		int a[] = { 1, 2, 4, 1 };
+		int b[] = { 3, 1, 4 };
+		int c[] = { 1, 4, 4 };
+		assertArrayEquals(new int[] { 1, 2 }, s1.pre4(a));
+		assertArrayEquals(new int[] { 3, 1 }, s1.pre4(b));
+		assertArrayEquals(new int[] { 1 }, s1.pre4(c));
+	}
+
+	@Test
+	void testl() {
+		CompareAgain s1 = new CompareAgain();
+		int a[] = { 1, 0, 0, 1 };
+		int b[] = { 0, 1, 1, 0, 1 };
+		int c[] = { 1, 0 };
+		assertArrayEquals(new int[] { 0, 0, 1, 1 }, s1.zeroFront(a));
+		assertArrayEquals(new int[] { 0, 0, 1, 1, 1 }, s1.zeroFront(b));
+		assertArrayEquals(new int[] { 0, 1 }, s1.zeroFront(c));
+	}
+
+	@Test
+	void testm() {
+		CompareAgain s1 = new CompareAgain();
+		int a[] = { 1, 0, 1, 0, 0, 1, 1 };
+		int b[] = { 3, 3, 2 };
+		int c[] = { 2, 2, 2 };
+		assertArrayEquals(new int[] { 0, 0, 0, 1, 1, 1, 1 }, s1.evenOdd(a));
+		assertArrayEquals(new int[] { 2, 3, 3 }, s1.evenOdd(b));
+		assertArrayEquals(new int[] { 2, 2, 2 }, s1.evenOdd(c));
+	}
+
+	@Test
+	void testn() {
+		CompareAgain s1 = new CompareAgain();
+		int a[] = { 1, 2, 1, 1, 3 };
+		int b[] = { 1, 4, 2, 1, 4, 1, 4 };
+		int c[] = { 1, 4, 2, 1, 4, 4, 4 };
+		assertEquals(s1.maxSpan(a), 4);
+		assertEquals(s1.maxSpan(b), 6);
+		assertEquals(s1.maxSpan(c), 6);
+	}
+
+	@Test
+	void testo() {
+		CompareAgain s1 = new CompareAgain();
+		int a[] = { 1, 1, 1, 2, 1 };
+		int b[] = { 2, 1, 1, 2, 1 };
+		int c[] = { 10, 10 };
+		assertEquals(s1.canBalance(a), true);
+		assertEquals(s1.canBalance(b), false);
+		assertEquals(s1.canBalance(c), true);
+	}
+
+	@Test
+	void testp() {
+		CompareAgain s1 = new CompareAgain();
+		assertArrayEquals(new int[] { 1, 1, 2, 1, 2, 3 }, s1.seriesUp(3));
+		assertArrayEquals(new int[] { 1, 1, 2, 1, 2, 3, 1, 2, 3, 4 }, s1.seriesUp(4));
+		assertArrayEquals(new int[] { 1, 1, 2 }, s1.seriesUp(2));
+	}
+
+	@Test
+	void testq() {
+		CompareAgain s1 = new CompareAgain();
+		int a[] = { 1, 3, 1, 4 };
+		int b[] = { 1, 3, 1, 4, 4, 3, 1 };
+		int c[] = { 3, 2, 2, 4 };
+		assertArrayEquals(new int[] { 1, 3, 4, 1 }, s1.fix34(a));
+		assertArrayEquals(new int[] { 1, 3, 4, 1, 1, 3, 4 }, s1.fix34(b));
+		assertArrayEquals(new int[] { 3, 4, 2, 2 }, s1.fix34(c));
+	}
+
+	@Test
+	void testr() {
+		CompareAgain s1 = new CompareAgain();
+		int a[] = { 1, 2, 4, 6 };
+		int b[] = { 2, 4 };
+		int a1[] = { 1, 2, 4, 6 };
+		int b1[] = { 2, 3, 4 };
+		int a2[] = { 1, 2, 4, 4, 6 };
+		int b2[] = { 2, 4 };
+		assertEquals(s1.linearIn(a, b), true);
+		assertEquals(s1.linearIn(a1, b1), false);
+		assertEquals(s1.linearIn(a2, b2), true);
+	}
+
+	@Test
+	void tests() {
+		CompareAgain s1 = new CompareAgain();
+		int a[] = { 1, 2, 3, 8, 9, 3, 2, 1 };
+		int b[] = { 1, 2, 1, 4 };
+		int c[] = { 7, 1, 2, 9, 7, 2, 1 };
+		assertEquals(s1.maxMirror(a), 3);
+		assertEquals(s1.maxMirror(b), 3);
+		assertEquals(s1.maxMirror(c), 2);
+	}
+
+	@Test
+	void testt() {
+		CompareAgain s1 = new CompareAgain();
+		int a[] = { 5, 4, 9, 4, 9, 5 };
+		int b[] = { 1, 4, 1, 5 };
+		int c[] = { 1, 4, 1, 5, 5, 4, 1 };
+		assertArrayEquals(new int[] { 9, 4, 5, 4, 5, 9 }, s1.fix45(a));
+		assertArrayEquals(new int[] { 1, 4, 5, 1 }, s1.fix45(b));
+		assertArrayEquals(new int[] { 1, 4, 5, 1, 1, 4, 5 }, s1.fix45(c));
+	}
 }

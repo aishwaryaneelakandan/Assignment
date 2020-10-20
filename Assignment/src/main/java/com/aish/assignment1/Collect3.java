7f22e6e6fe8eb0439ecb2e445f4a2516a46017c9
DELL <DELL@192.168.1.6>
2020-10-20 17:45:39 +0530

    Commit Fifty

--- Assignment/src/main/java/com/aish/assignment1/Collect3.java
+++ Assignment/src/main/java/com/aish/assignment1/Collect3.java
@@ -1,35 +1,73 @@
 package com.aish.assignment1;
+
 import java.util.LinkedList;
 import java.util.Collections;
-public class Collect3 {
-public LinkedList<String> elementsposition(LinkedList<String> list){
-	for(int p=0; p < list.size(); p++) {
-		list.get(p);
-	}
-	return list;
-}
-public LinkedList<String> remove(LinkedList<String> list){
-	LinkedList<String> listOne=new LinkedList<String>();
-	listOne.addAll(list);
-	listOne.remove();
-	return listOne;
-}
-public LinkedList<String> removefirstlast(LinkedList<String> list){
-	LinkedList<String> listOne=new LinkedList<String>();
-	listOne.addAll(list);
-	listOne.removeFirst();
-	listOne.removeLast();
-	return listOne;
-}
-public LinkedList<String> removeall(LinkedList<String> list){
-	LinkedList<String> listOne=new LinkedList<String>();
-	listOne.addAll(list);
-	listOne.removeAll(list);
-	return listOne;
-}
-public LinkedList<String> swap(LinkedList<String> list){
-	Collections.swap(list, 1, 2);
-	return list;
-}
 
+public class Collect3 {
+	public LinkedList<String> elementsposition(LinkedList<String> list) {
+		for (int p = 0; p < list.size(); p++) {
+			list.get(p);
+		}
+		return list;
+	}
+
+	public LinkedList<String> remove(LinkedList<String> list) {
+		LinkedList<String> listOne = new LinkedList<String>();
+		listOne.addAll(list);
+		listOne.remove();
+		return listOne;
+	}
+
+	public LinkedList<String> removefirstlast(LinkedList<String> list) {
+		LinkedList<String> listOne = new LinkedList<String>();
+		listOne.addAll(list);
+		listOne.removeFirst();
+		listOne.removeLast();
+		return listOne;
+	}
+
+	public LinkedList<String> removeall(LinkedList<String> list) {
+		LinkedList<String> listOne = new LinkedList<String>();
+		listOne.addAll(list);
+		listOne.removeAll(list);
+		return listOne;
+	}
+
+	public LinkedList<String> swap(LinkedList<String> list) {
+		Collections.swap(list, 1, 2);
+		return list;
+	}
+
+	public LinkedList<String> shuffle(LinkedList<String> list) {
+		Collections.shuffle(list);
+		return list;
+	}
+
+	public LinkedList<String> join(LinkedList<String> list1, LinkedList<String> list2) {
+		LinkedList<String> listOne = new LinkedList<String>();
+		listOne.addAll(list1);
+		listOne.addAll(list2);
+		return listOne;
+	}
+
+	public LinkedList<String> clone(LinkedList<String> list) {
+		LinkedList<String> listOne = new LinkedList<String>();
+		listOne.clone();
+		listOne.addAll(list);
+		return listOne;
+	}
+
+	public LinkedList<String> isempty(LinkedList<String> list) {
+		LinkedList<String> listOne = new LinkedList<String>();
+		listOne.addAll(list);
+		listOne.isEmpty();
+		return listOne;
+	}
+
+	public LinkedList<String> replace(LinkedList<String> list, String a1) {
+		LinkedList<String> listOne = new LinkedList<String>();
+		listOne.addAll(list);
+		listOne.set(0, a1);
+		return listOne;
+	}
 }
--- Assignment/src/test/java/com/aish/assignment1/Collect3Test.java
+++ Assignment/src/test/java/com/aish/assignment1/Collect3Test.java
@@ -3,7 +3,6 @@ import java.util.LinkedList;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
 class Collect3Test {
 
 	@Test
@@ -64,5 +63,70 @@ class Collect3Test {
 		list1.add("Blue");
 		list1.add("Green");
 		assertEquals(s6.swap(list),list1);
+	}
+	@Test
+	void Shuffle() {
+		Collect3 s6=new Collect3();
+		LinkedList<String> list=new LinkedList<String>();
+		list.add("Red");
+		list.add("Green");
+		LinkedList<String> list1=new LinkedList<String>();
+		list1.add("Green");
+		list1.add("Red");
+		assertEquals(s6.shuffle(list),list1);
+	}
+	@Test
+	void Join() {
+		Collect3 s6=new Collect3();
+		LinkedList<String> list=new LinkedList<String>();
+		list.add("Red");
+		list.add("Green");
+		list.add("Blue");
+		LinkedList<String> list1=new LinkedList<String>();
+		list1.add( "Yellow");
+		list1.add( "Orange");
+		list1.add( "Purple");
+		LinkedList<String> list2=new LinkedList<String>();
+		list2.add("Red");
+		list2.add("Green");
+		list2.add("Blue");
+		list2.add("Yellow");
+		list2.add("Orange");
+		list2.add("Purple");
+		assertEquals(s6.join(list, list1),list2);
+	}
+	@Test
+	void Clone() {
+		Collect3 s6=new Collect3();
+		LinkedList<String> list=new LinkedList<String>();
+		list.add("Red");
+		list.add("Green");
+		list.add("Blue");
+		LinkedList<String> list1=new LinkedList<String>();
+		list1.add("Red");
+		list1.add("Green");
+		list1.add("Blue");
+		assertEquals(s6.clone(list),list1);
+	}
+@Test
+	void Isempty() {
+		Collect3 s6=new Collect3();
+		LinkedList<String> list=new LinkedList<String>();
+		LinkedList<String> list1=new LinkedList<String>();
+		assertEquals(s6.removeall(list),list1);
+	}
+	@Test
+	void Replace() {
+		Collect3 s6=new Collect3();
+		LinkedList<String> list=new LinkedList<String>();
+		String a="White";
+		list.add("Red");
+		list.add("Green");
+		list.add("Blue");
+		LinkedList<String> list1=new LinkedList<String>();
+		list1.add("White");
+		list1.add("Green");
+		list1.add("Blue");
+		assertEquals(s6.replace(list, a),list1);
 	}
 }

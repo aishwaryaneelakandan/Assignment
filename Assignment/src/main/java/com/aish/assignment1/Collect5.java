c6ec8bce28913a5a68d9252b56d92608cc1f927e
DELL <DELL@192.168.1.6>
2020-10-23 18:05:09 +0530

    Commit Fifty Six

--- Assignment/src/main/java/com/aish/assignment1/Collect5.java
+++ Assignment/src/main/java/com/aish/assignment1/Collect5.java
@@ -24,5 +24,34 @@ public class Collect5 {
 		return res;
 	}
 
-	
+	public TreeSet<String> clone(TreeSet<String> t1) {
+		TreeSet<String> t2 = new TreeSet<String>();
+		t2.addAll(t1);
+		t2.clone();
+		return t2;
+	}
+
+	public int size(TreeSet<String> t1) {
+		int t2 = 0;
+		t2 = t1.size();
+		return t2;
+	}
+
+	public boolean compare(TreeSet<String> t1, TreeSet<String> t2) {
+		if (t1.containsAll(t2))
+			return true;
+		else
+			return false;
+	}
+
+	public Integer returnfirst(TreeSet<Integer> t1) {
+		int a = t1.pollFirst();
+		return a;
+	}
+
+	public Integer returnlast(TreeSet<Integer> t1) {
+		int a = t1.pollLast();
+		return a;
+	}
+
 }
--- Assignment/src/test/java/com/aish/assignment1/Collect5Test.java
+++ Assignment/src/test/java/com/aish/assignment1/Collect5Test.java
@@ -1,7 +1,8 @@
 package com.aish.assignment1;
 
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.TreeSet;
 
 import org.junit.jupiter.api.Test;
@@ -70,4 +71,64 @@ class Collect5Test {
 		t2.add(10);
 		assertEquals(s8.reverse(t1), t2);
 	}
+	@Test
+	void Clone() {
+		Collect5 s8 = new Collect5();
+		TreeSet<String> t1 = new TreeSet<String>();
+		t1.add("Red");
+		t1.add("Green");
+		t1.add("Blue");
+		TreeSet<String> t2 = new TreeSet<String>();
+		t2.add("Red");
+		t2.add("Green");
+		t2.add("Blue");
+		assertEquals(s8.clone(t1), t2);
+	}
+
+	@Test
+	void Size() {
+		Collect5 s8 = new Collect5();
+		TreeSet<String> set1 = new TreeSet<String>();
+		set1.add("Red");
+		set1.add("Blue");
+		set1.add("Green");
+		assertEquals(s8.size(set1), 3);
+	}
+
+	@Test
+	void Comapre() {
+		Collect5 s8 = new Collect5();
+		TreeSet<String> t1 = new TreeSet<String>();
+		t1.add("Red");
+		t1.add("Green");
+		t1.add("Blue");
+		TreeSet<String> t2 = new TreeSet<String>();
+		t2.add("Red");
+		t2.add("Green");
+		t2.add("Blue");
+		assertTrue(s8.compare(t1, t2));
+	}
+
+	@Test
+	void Returnfirst() {
+		Collect5 s8 = new Collect5();
+		TreeSet<Integer> t1 = new TreeSet<Integer>();
+		t1.add(10);
+		t1.add(20);
+		t1.add(30);
+		int a1 = 10;
+		assertEquals(s8.returnfirst(t1), a1);
+	}
+
+	@Test
+	void Returnlast() {
+		Collect5 s8 = new Collect5();
+		TreeSet<Integer> t1 = new TreeSet<Integer>();
+		t1.add(10);
+		t1.add(20);
+		t1.add(30);
+		int a1 = 30;
+		assertEquals(s8.returnlast(t1), a1);
+	}
+	
 }

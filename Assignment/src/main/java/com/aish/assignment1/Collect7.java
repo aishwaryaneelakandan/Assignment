174711e3fa952e708ad1b025f3bc1b19473c6400
DELL <DELL@192.168.1.6>
2020-10-29 18:11:08 +0530

    Commit Sixty One

--- Assignment/src/main/java/com/aish/assignment1/Collect7.java
+++ Assignment/src/main/java/com/aish/assignment1/Collect7.java
@@ -2,6 +2,7 @@ package com.aish.assignment1;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Collect7 {
 	public HashMap<Integer, String> associate(HashMap<Integer, String> map) {
@@ -37,4 +38,34 @@ public class Collect7 {
 		return map1;
 	}
 
+	public HashMap<Integer, String> clone(HashMap<Integer, String> map) {
+		HashMap<Integer, String> map1 = new HashMap<Integer, String>();
+		map1.putAll(map);
+		map1.clone();
+		return map1;
+	}
+
+	public boolean specifiedkey(HashMap<Integer, String> map, Integer a) {
+		if (map.containsKey(a))
+			return true;
+		else
+			return false;
+	}
+
+	public boolean specifiedvalue(HashMap<Integer, String> map, String a) {
+		if (map.containsValue(a))
+			return true;
+		else
+			return false;
+	}
+
+	public String getkey(HashMap<Integer, String> map, int a) {
+		String val = (String) map.get(a);
+		return val;
+	}
+
+	public Set<Integer> setvalue(HashMap<Integer, String> map) {
+		Set<Integer> keyset = map.keySet();
+		return keyset;
+	}
 }
--- Assignment/src/test/java/com/aish/assignment1/Collect7Test.java
+++ Assignment/src/test/java/com/aish/assignment1/Collect7Test.java
@@ -1,7 +1,11 @@
 package com.aish.assignment1;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
@@ -78,4 +82,78 @@ class Collect7Test {
 		assertEquals(s10.isemptyornot(map1), map2);
 	}
 
+	@Test
+	void Clone() {
+		Collect7 s10 = new Collect7();
+		HashMap<Integer, String> map1 = new HashMap<>();
+		map1.put(1, "Red");
+		map1.put(2, "Green");
+		map1.put(3, "Black");
+		map1.put(4, "White");
+		map1.put(5, "Blue");
+		HashMap<Integer, String> map2 = new HashMap<>();
+		map2.put(1, "Red");
+		map2.put(2, "Green");
+		map2.put(3, "Black");
+		map2.put(4, "White");
+		map2.put(5, "Blue");
+		assertEquals(s10.clone(map1), map2);
+	}
+
+	@Test
+	void SpecifiedKey() {
+		Collect7 s10 = new Collect7();
+		HashMap<Integer, String> map1 = new HashMap<>();
+		Integer a = 1;
+		map1.put(1, "Red");
+		map1.put(2, "Green");
+		map1.put(3, "Black");
+		map1.put(4, "White");
+		map1.put(5, "Blue");
+		assertTrue(s10.specifiedkey(map1, a));
+	}
+
+	@Test
+	void SpecifiedValue() {
+		Collect7 s10 = new Collect7();
+		HashMap<Integer, String> map1 = new HashMap<>();
+		String a = "Black";
+		map1.put(1, "Red");
+		map1.put(2, "Green");
+		map1.put(3, "Black");
+		map1.put(4, "White");
+		map1.put(5, "Blue");
+		assertTrue(s10.specifiedvalue(map1, a));
+	}
+
+	@Test
+	void GetKey() {
+		Collect7 s10 = new Collect7();
+		HashMap<Integer, String> map1 = new HashMap<>();
+		map1.put(1, "Red");
+		map1.put(2, "Green");
+		map1.put(3, "Black");
+		map1.put(4, "White");
+		map1.put(5, "Blue");
+		int a = 3;
+		assertEquals(s10.getkey(map1, a), map1.put(3, "Black"));
+	}
+
+	@Test
+	void SetValue() {
+		Collect7 s10 = new Collect7();
+		HashMap<Integer, String> map1 = new HashMap<>();
+		map1.put(1, "Red");
+		map1.put(2, "Green");
+		map1.put(3, "Black");
+		map1.put(4, "White");
+		map1.put(5, "Blue");
+		Set<Integer> map2 = new HashSet<>();
+		map2.add(1);
+		map2.add(2);
+		map2.add(3);
+		map2.add(4);
+		map2.add(5);
+		assertEquals(s10.setvalue(map1), map2);
+	}
 }

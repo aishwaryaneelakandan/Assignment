dde5555eaf43c346c276b4b3039cb171ef8efd9b
DELL <DELL@192.168.1.6>
2020-10-30 20:58:31 +0530

    Commit Sixty Four

--- Assignment/src/main/java/com/aish/assignment1/Collect8.java
+++ Assignment/src/main/java/com/aish/assignment1/Collect8.java
@@ -2,66 +2,122 @@ package com.aish.assignment1;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class Collect8 {
-	public TreeMap<Integer,String> associate(TreeMap<Integer,String> tree){
-		for (Map.Entry<Integer,String> entry : tree.entrySet()) {
+	public TreeMap<Integer, String> associate(TreeMap<Integer, String> tree) {
+		for (Map.Entry<Integer, String> entry : tree.entrySet()) {
 			entry.getKey();
 			entry.getValue();
 		}
 		return tree;
 	}
-	public TreeMap<Integer,String> copy(TreeMap<Integer,String> tree,TreeMap<Integer,String> tree1){
+
+	public TreeMap<Integer, String> copy(TreeMap<Integer, String> tree, TreeMap<Integer, String> tree1) {
 		tree1.putAll(tree);
 		return tree1;
 	}
-	public boolean searchkey(TreeMap<Integer,String> tree,Integer a){
+
+	public boolean searchkey(TreeMap<Integer, String> tree, Integer a) {
 		if (tree.containsKey(a))
 			return true;
 		else
 			return false;
 	}
-	public boolean searchvalue(TreeMap<Integer,String> tree,String a){
+
+	public boolean searchvalue(TreeMap<Integer, String> tree, String a) {
 		if (tree.containsValue(a))
 			return true;
 		else
 			return false;
 	}
-	public Set<Integer> getkeys(TreeMap<Integer,String> tree){
+
+	public Set<Integer> getkeys(TreeMap<Integer, String> tree) {
 		Set<Integer> keyset = tree.keySet();
 		return keyset;
 	}
-	public TreeMap<Integer,String> delete(TreeMap<Integer,String> tree){
-		TreeMap<Integer,String> tree1=new TreeMap<Integer,String>();
+
+	public TreeMap<Integer, String> delete(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
 		tree1.putAll(tree);
 		tree1.clear();
 		return tree1;
 	}
-	public TreeMap<Integer,String> getkeyvalue(TreeMap<Integer,String> tree){
-		TreeMap<Integer,String> tree1=new TreeMap<Integer,String>();
+
+	public TreeMap<Integer, String> getkeyvalue(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
 		tree1.firstEntry();
 		tree1.putAll(tree);
 		tree1.lastEntry();
 		return tree1;
-}
-	public TreeMap<Integer,String> gethighestlowestkey(TreeMap<Integer,String> tree){
-		TreeMap<Integer,String> tree1=new TreeMap<Integer,String>();
+	}
+
+	public TreeMap<Integer, String> gethighestlowestkey(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
 		tree1.firstKey();
 		tree1.putAll(tree);
 		tree1.lastKey();
 		return tree1;
-}
-	
-	public TreeMap<Integer,String> greaterorlesser(TreeMap<Integer,String> tree){
-		TreeMap<Integer,String> tree1=new TreeMap<Integer,String>();
+	}
+
+	public TreeMap<Integer, String> greaterorlesser(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
 		tree1.floorEntry(10);
 		return tree1;
-}
-	public TreeMap<Integer,String> strictlyless(TreeMap<Integer,String> tree){
-		TreeMap<Integer,String> tree1=new TreeMap<Integer,String>();
+	}
+
+	public TreeMap<Integer, String> strictlyless(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
 		tree1.headMap(20);
 		return tree1;
-}
-	
+	}
+
+	public TreeMap<Integer, String> inclusivetrue(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
+		tree1.headMap(50, true);
+		return tree1;
+	}
+
+	public TreeMap<Integer, String> leastkeygreaterthanorequal(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
+		tree1.ceilingEntry(20);
+		return tree1;
+	}
+
+	public TreeMap<Integer, String> removeleastkey(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
+		tree1.pollFirstEntry();
+		return tree1;
+	}
+
+	public TreeMap<Integer, String> removegreatestkey(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
+		tree1.pollLastEntry();
+		return tree1;
+	}
+
+	public SortedMap<Integer, String> inclusiveexclusive(TreeMap<Integer, String> tree) {
+		SortedMap<Integer, String> sub_tree_map = new TreeMap<Integer, String>();
+		sub_tree_map = tree.subMap(20, 40);
+		return sub_tree_map;
+	}
+
+	public SortedMap<Integer, String> getportion(TreeMap<Integer, String> tree) {
+		SortedMap<Integer, String> sub_tree_map = new TreeMap<Integer, String>();
+		sub_tree_map = tree.subMap(20, true, 40, true);
+		return sub_tree_map;
+	}
+
+	public TreeMap<Integer, String> greaterthanequalto(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
+		tree1.tailMap(20);
+		return tree1;
+	}
+
+	public TreeMap<Integer, String> greaterthan(TreeMap<Integer, String> tree) {
+		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
+		tree1.tailMap(20, false);
+		return tree1;
+	}
 }
--- Assignment/src/test/java/com/aish/assignment1/Collect8Test.java
+++ Assignment/src/test/java/com/aish/assignment1/Collect8Test.java
@@ -13,8 +13,8 @@ class Collect8Test {
 
 	@Test
 	void Associate() {
-		Collect8 s11=new Collect8();
-		TreeMap<Integer,String> map1=new TreeMap<Integer,String>();
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
 		map1.put(1, "Red");
 		map1.put(2, "Green");
 		map1.put(3, "Black");
@@ -27,18 +27,19 @@ class Collect8Test {
 		assertEquals(map1.get(4), "White");
 		assertEquals(map1.get(5), "Blue");
 	}
+
 	@Test
 	void Copy() {
-		Collect8 s11=new Collect8();
-		TreeMap<Integer,String> map1=new TreeMap<Integer,String>();
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
 		map1.put(1, "Red");
 		map1.put(2, "Green");
 		map1.put(3, "Black");
-		TreeMap<Integer,String> map2=new TreeMap<Integer,String>();
+		TreeMap<Integer, String> map2 = new TreeMap<Integer, String>();
 		map2.put(4, "White");
 		map2.put(5, "Blue");
 		map2.put(6, "Orange");
-		TreeMap<Integer,String> map3=new TreeMap<Integer,String>();
+		TreeMap<Integer, String> map3 = new TreeMap<Integer, String>();
 		map3.put(1, "Red");
 		map3.put(2, "Green");
 		map3.put(3, "Black");
@@ -47,11 +48,12 @@ class Collect8Test {
 		map3.put(6, "Orange");
 		assertEquals(s11.copy(map1, map2), map3);
 	}
+
 	@Test
 	void SearchKey() {
-		Collect8 s11=new Collect8();
-		TreeMap<Integer,String> map1=new TreeMap<Integer,String>();
-		Integer a=3;
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		Integer a = 3;
 		map1.put(1, "Red");
 		map1.put(2, "Green");
 		map1.put(3, "Black");
@@ -59,11 +61,12 @@ class Collect8Test {
 		map1.put(5, "Blue");
 		assertTrue(s11.searchkey(map1, a));
 	}
+
 	@Test
 	void SearchValue() {
-		Collect8 s11=new Collect8();
-		TreeMap<Integer,String> map1=new TreeMap<Integer,String>();
-		String a="Blue";
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		String a = "Blue";
 		map1.put(1, "Red");
 		map1.put(2, "Green");
 		map1.put(3, "Black");
@@ -71,10 +74,11 @@ class Collect8Test {
 		map1.put(5, "Blue");
 		assertTrue(s11.searchvalue(map1, a));
 	}
+
 	@Test
 	void GetKeys() {
-		Collect8 s11=new Collect8();
-		TreeMap<Integer,String> map1=new TreeMap<Integer,String>();
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
 		map1.put(1, "Red");
 		map1.put(2, "Green");
 		map1.put(3, "Black");
@@ -88,51 +92,159 @@ class Collect8Test {
 		map2.add(5);
 		assertEquals(s11.getkeys(map1), map2);
 	}
+
 	@Test
 	void Delete() {
-		Collect8 s11=new Collect8();
-		TreeMap<Integer,String> map1=new TreeMap<Integer,String>();
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
 		map1.put(1, "Red");
 		map1.put(2, "Green");
 		map1.put(3, "Black");
 		map1.put(4, "White");
 		map1.put(5, "Blue");
-		TreeMap<Integer,String> map2=new TreeMap<Integer,String>();
+		TreeMap<Integer, String> map2 = new TreeMap<Integer, String>();
 		assertEquals(s11.delete(map1), map2);
-}
+	}
+
 	@Test
 	void GetKeyValue() {
-		Collect8 s11=new Collect8();
-		TreeMap<Integer,String> map1=new TreeMap<Integer,String>();
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
 		map1.put(1, "Red");
 		map1.put(2, "Green");
 		map1.put(3, "Black");
 		map1.put(4, "White");
 		s11.getkeyvalue(map1);
-		assertEquals(map1.get(1),"Red");
-		assertEquals(map1.get(4),"White");
-}
+		assertEquals(map1.get(1), "Red");
+		assertEquals(map1.get(4), "White");
+	}
+
 	@Test
 	void GreaterOrLesser() {
-		Collect8 s11=new Collect8();
-		TreeMap<Integer,String> map1=new TreeMap<Integer,String>();
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
 		map1.put(10, "Red");
 		map1.put(20, "Green");
 		map1.put(30, "Black");
 		map1.put(40, "White");
 		s11.greaterorlesser(map1);
-		assertEquals(map1.get(10),"Red");
-}
+		assertEquals(map1.get(10), "Red");
+	}
+
 	@Test
 	void StrictlyLess() {
-		Collect8 s11=new Collect8();
-		TreeMap<Integer,String> map1=new TreeMap<Integer,String>();
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
 		map1.put(1, "Red");
 		map1.put(20, "Green");
 		map1.put(30, "Black");
 		map1.put(40, "White");
 		s11.strictlyless(map1);
-		assertEquals(map1.get(1),"Red");
-}
-	
+		assertEquals(map1.get(1), "Red");
+	}
+
+	@Test
+	void Inclusivetrue() {
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		map1.put(1, "Red");
+		map1.put(20, "Green");
+		map1.put(30, "Black");
+		map1.put(40, "White");
+		s11.inclusivetrue(map1);
+		assertEquals(map1.get(1), "Red");
+		assertEquals(map1.get(20), "Green");
+		assertEquals(map1.get(30), "Black");
+		assertEquals(map1.get(40), "White");
+	}
+
+	@Test
+	void LeastKeyGreaerThanOrEqual() {
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		map1.put(1, "Red");
+		map1.put(20, "Green");
+		map1.put(30, "Black");
+		map1.put(40, "White");
+		s11.leastkeygreaterthanorequal(map1);
+		assertEquals(map1.get(20), "Green");
+	}
+
+	@Test
+	void RemoveLeastKey() {
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		map1.put(1, "Red");
+		map1.put(20, "Green");
+		map1.put(30, "Black");
+		map1.put(40, "White");
+		s11.removeleastkey(map1);
+		assertEquals(map1.get(1), "Red");
+	}
+
+	@Test
+	void RemoveGreatestKey() {
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		map1.put(1, "Red");
+		map1.put(20, "Green");
+		map1.put(30, "Black");
+		map1.put(40, "White");
+		s11.removegreatestkey(map1);
+		assertEquals(map1.get(40), "White");
+	}
+
+	@Test
+	void InclusiveExclusive() {
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		map1.put(1, "Red");
+		map1.put(20, "Green");
+		map1.put(30, "Black");
+		map1.put(40, "White");
+		s11.inclusiveexclusive(map1);
+		assertEquals(map1.get(20), "Green");
+		assertEquals(map1.get(30), "Black");
+	}
+
+	@Test
+	void GetPortion() {
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		map1.put(1, "Red");
+		map1.put(20, "Green");
+		map1.put(30, "Black");
+		map1.put(40, "White");
+		s11.getportion(map1);
+		assertEquals(map1.get(20), "Green");
+		assertEquals(map1.get(30), "Black");
+		assertEquals(map1.get(40), "White");
+	}
+
+	@Test
+	void GreaterThanEqualTo() {
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		map1.put(1, "Red");
+		map1.put(20, "Green");
+		map1.put(30, "Black");
+		map1.put(40, "White");
+		s11.greaterthanequalto(map1);
+		assertEquals(map1.get(20), "Green");
+		assertEquals(map1.get(30), "Black");
+		assertEquals(map1.get(40), "White");
+	}
+
+	@Test
+	void GreaterThan() {
+		Collect8 s11 = new Collect8();
+		TreeMap<Integer, String> map1 = new TreeMap<Integer, String>();
+		map1.put(1, "Red");
+		map1.put(20, "Green");
+		map1.put(30, "Black");
+		map1.put(40, "White");
+		s11.greaterthan(map1);
+		assertEquals(map1.get(30), "Black");
+		assertEquals(map1.get(40), "White");
+	}
 }

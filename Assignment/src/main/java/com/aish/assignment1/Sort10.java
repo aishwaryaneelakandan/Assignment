a89428210d99dac24c8e36d19f239ebb55121d34
DELL <DELL@192.168.1.6>
2020-11-18 22:29:26 +0530

    Commit Sixty Eight

---
+++ Assignment/src/main/java/com/aish/assignment1/Sort10.java
@@ -0,0 +1,100 @@
+package com.aish.assignment1;
+
+public class Sort10 {
+	static int largest(int a[]) {
+		int larger = a[0], i;
+		for (i = 1; i < 10; i++) {
+			if (a[i] > larger)
+				larger = a[i];
+		}
+		return larger;
+	}
+
+	static void frequency(int a[]) {
+		int bucket[][] = new int[10][10];
+		int bucket_count[] = new int[10];
+		int i, j, k, remainder, NOP = 0, divisor = 1, larger, pass;
+		larger = largest(a);
+		while (larger > 0) {
+			NOP++;
+			larger /= 10;
+		}
+		for (pass = 0; pass < NOP; pass++) {
+			for (i = 0; i < 10; i++)
+				bucket_count[i] = 0;
+			for (i = 0; i < 10; i++) {
+				remainder = (a[i] / divisor) % 10;
+				bucket[remainder][bucket_count[remainder]] = a[i];
+				bucket_count[remainder] += 1;
+			}
+			i = 0;
+			for (k = 0; k < 10; k++) {
+				for (j = 0; j < bucket_count[k]; j++) {
+					a[i] = bucket[k][j];
+					i++;
+				}
+			}
+			divisor *= 10;
+		}
+	}
+
+	public int[] q3(int a[]) {
+		frequency(a);
+		return a;
+	}
+
+	static int getMedian(int ar1[], int ar2[], int n) {
+		int i = 0;
+		int j = 0;
+		int count;
+		int m1 = -1, m2 = -1;
+		for (count = 0; count <= n; count++) {
+			if (i == n) {
+				m1 = m2;
+				m2 = ar2[0];
+				break;
+			} else if (j == n) {
+				m1 = m2;
+				m2 = ar1[0];
+				break;
+			}
+			if (ar1[i] <= ar2[j]) {
+				m1 = m2;
+				m2 = ar1[i];
+				i++;
+			} else {
+				m1 = m2;
+				m2 = ar2[j];
+				j++;
+			}
+		}
+		return (m1 + m2) / 2;
+	}
+
+	public boolean q2(int ar1[], int ar2[], int n) {
+		int n1 = ar1.length;
+		int n2 = ar2.length;
+		if (n1 == n2)
+			return true;
+		else
+			return false;
+	}
+
+	static int maxRepeating(int arr[], int n, int k) {
+		for (int i = 0; i < n; i++)
+			arr[(arr[i] % k)] += k;
+		int max = arr[0], result = 0;
+		for (int i = 1; i < n; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+				result = i;
+			}
+		}
+		return result;
+	}
+
+	public int q4(int arr[], int n, int k) {
+		maxRepeating(arr, n, k);
+		return n;
+	}
+}
---
+++ Assignment/src/test/java/com/aish/assignment1/Sort10Test.java
@@ -0,0 +1,32 @@
+package com.aish.assignment1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Sort10Test {
+
+	@Test
+	void test1() {
+		Sort10 s10 = new Sort10();
+		int a[] = { 90, 23, 101, 45, 65, 23, 67, 89, 34, 23 };
+		assertArrayEquals(new int[] { 23, 23, 23, 34, 45, 65, 67, 89, 90, 101 }, s10.q3(a));
+	}
+
+	@Test
+	void test2() {
+		Sort10 s10 = new Sort10();
+		int a[] = { 1, 12, 15, 26, 38 };
+		int a1[] = { 2, 13, 17, 30, 45 };
+		int n = 16;
+		assertTrue(s10.q2(a, a1, n));
+	}
+
+	@Test
+	void test4() {
+		Sort10 s10 = new Sort10();
+		int a[] = { 2, 3, 5, 3, 7, 9, 5, 3, 7 };
+		int n = 3;
+		assertEquals(s10.q4(a, n, 8), 3);
+	}
+}
